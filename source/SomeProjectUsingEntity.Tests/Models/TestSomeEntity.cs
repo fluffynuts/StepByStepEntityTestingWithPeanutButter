4b2d4f3e1b6071312b367770790270ff71173248
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-01-12 16:43:18 +0200

    * add first test, fails for the right reason: have no Id property * also note that I had to update some packages which were brought     in as dependencies since I had previously failed to specify     correct package dependencies in PeanutButter packages. I updated:     Nunit     NSubstitute     if you see errors like : System.IO.FileLoadException : Could not load file or assembly 'nunit.framework, Version=3.0.5813.39031, Culture=neutral, PublicKeyToken=2638cd05610744eb' or one of its dependencies. The located assembly's manifest definition does not match the assembly reference. (Exception from HRESULT: 0x80131040)
       at PeanutButter.TestUtils.Generic.TypeExtensions.ShouldHaveProperty(Type type, String name, Type ofType, Boolean shouldBeVirtual)
       at SomeProjectUsingEntity.Tests.Models.TestSomeEntity.Type_ShouldHaveProperty_(String propertyName, Type propertyType) in C:\code\StepByStepEntityTestingWithPeanutButter\source\SomeProjectUsingEntity.Tests\Models\TestSomeEntity.cs:line 25
    
        in your test output, this is the culprit. Versions of PeanutButter
        packages 1.1.9 and later should have this fixed.

--- source/SomeProjectUsingEntity.Tests/Models/TestSomeEntity.cs
+++ source/SomeProjectUsingEntity.Tests/Models/TestSomeEntity.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using PeanutButter.TestUtils.Generic;
+using SomeProjectUsingEntity.Models;
 
 namespace SomeProjectUsingEntity.Tests.Models
 {
+    [TestFixture]
     public class TestSomeEntity
     {
+        [TestCase("Id", typeof(int))]
+        public void Type_ShouldHaveProperty_(string propertyName, Type propertyType)
+        {
+            //---------------Set up test pack-------------------
+            var sut = typeof (SomeEntity);
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            sut.ShouldHaveProperty(propertyName, propertyType);
+
+            //---------------Test Result -----------------------
+        }
+
     }
 }
