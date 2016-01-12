47d3c8ec01f2b26e4290032bb2b077a2f4060008
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-01-12 16:29:09 +0200

    * add a test fixture stub to SomeProjectUsingEntity.Tests * install nuget package peanutbutter.testutils.entity

---
+++ source/SomeProjectUsingEntity.Tests/Models/TestSomeEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeProjectUsingEntity.Tests.Models
+{
+    public class TestSomeEntity
+    {
+    }
+}
