91c68dde989817ca741308042ccf616ec135406b
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-01-12 16:48:00 +0200

    * Require the Name property

--- source/SomeProjectUsingEntity/Models/SomeEntity.cs
+++ source/SomeProjectUsingEntity/Models/SomeEntity.cs
@@ -1,8 +1,11 @@
-﻿namespace SomeProjectUsingEntity.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SomeProjectUsingEntity.Models
 {
     public class SomeEntity
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Notes { get; set; }
     }
