ec977d03752e0fcabdb72a55a4dcf43170c8761c
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-01-19 10:26:15 +0200

    * add test enforcing that EF migrations aren't being used

---
+++ source/SomeProjectUsingEntity.Tests/Models/TestSomeContext.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace SomeProjectUsingEntity.Tests.Models
+{
+    [TestFixture]
+    public class TestSomeContext: SomeContextPersistenceTestFixtureBase
+    {
+        [Test]
+        public void ShouldNotAllowEntityMigrations()
+        {
+            using (var ctx = GetContext())
+            {
+                //---------------Set up test pack-------------------
+                //---------------Assert Precondition----------------
+
+                //---------------Execute Test ----------------------
+                using (var cmd = ctx.Database.Connection.CreateCommand())
+                {
+                    cmd.CommandText = @"select * from INFORMATION_SCHEMA.TABLES where TABLE_NAME = '__MigrationHistory';";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        Assert.IsFalse(reader.Read(), "Should not have a __MigrationHistory table when EF migrations are disabled");
+                    }
+                    //---------------Test Result -----------------------
+                }
+            }
+        }
+
+    }
+}
